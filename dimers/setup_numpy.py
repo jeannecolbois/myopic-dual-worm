a039071bc2a510df4a2c13980decdf5abc6e68dc
Jeanne Colbois <jeanne.colbois@gmail.com>
2019-10-16 18:10:08 +0200

    Dimers compiling properly

--- dimers/setup_numpy.py
+++ dimers/setup_numpy.py
@@ -2,19 +2,14 @@
 
 import numpy as np
 from distutils.core import setup, Extension
-#from numpy.distutils.core import Extension
-#from numpy.distutils.misc_util import setup
-#import numpy.distutils.misc_util
 
-#links =numpy.distutils.misc_util.get_numpy_include_dirs();
-#links.append('-lgomp')
 cext = Extension(name = "dimers",
-                sources=["dimers_numpy.cpp", "hamiltonian.cpp","manydualworms.cpp", "mcsevolve.cpp", "updatespinstates.cpp"],
+                sources=["dimers.cpp", "hamiltonian.cpp","manydualworms.cpp", "mcsevolve.cpp", "updatespinstates.cpp"],
                 extra_compile_args=['-std=c++11', '-fopenmp'],
+                include_dirs = [np.get_include()],
                 extra_link_args=['-lgomp'],
                 language="c++")
 
 
 if __name__=="__main__":
 	setup(ext_modules=[cext], include_dirs = [np.get_include()])
-#, include_dirs=numpy.distutils.misc_util.get_numpy_include_dirs())
