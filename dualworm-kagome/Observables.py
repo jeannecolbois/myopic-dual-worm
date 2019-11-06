87cd42c09d1b6a39da2716edaa396c9c4125dfdd
Jeanne Colbois <jeanne.colbois@gmail.com>
2019-11-06 16:55:46 +0100

    Modified observables so as to take up less space

--- dualworm-kagome/Observables.py
+++ dualworm-kagome/Observables.py
@@ -12,7 +12,7 @@ import numpy as np
 # In[ ]:
 
 
-def energy(stlen, state, en_state, spinstate, s_ijl, ijl_s,c2s, csign, **kwargs):
+def energy(stlen, state, en_state, spinstate, s_ijl, ijl_s, **kwargs):
     return en_state/stlen
 
 
@@ -40,7 +40,7 @@ def centralcorrelations(stlen, state, en_state, spinstate, s_ijl, ijl_s,**kwargs
 
 
 def si(stlen, state, en_state, spinstate, s_ijl, ijl_s,**kwargs):
-    return spinstate
+    return np.array(spinstate, dtype = 'int8')
 
 def firstcorrelations(stlen, state, en_state, spinstate, s_ijl, ijl_s, nnlists = [], m = 0, **kwargs):
     mnow = [[sum([spinstate[s1] for (s1, s2) in nnlist])/len(nnlist),
@@ -51,8 +51,8 @@ def firstcorrelations(stlen, state, en_state, spinstate, s_ijl, ijl_s, nnlists =
     return np.array(firstcorr)
 
 def charges(stlen, state, en_state, spinstate, s_ijl, ijl_s,c2s = [], csign= [], **kwargs):
-    cvals = np.array([csign[c]*(spinstate[s1]+spinstate[s2]+spinstate[s3]) for c, (s1,s2,s3) in enumerate(c2s)])
-    #print(sum(abs(cvals)==3)/len(c2s))
+    cvals = np.array([csign[c]*(spinstate[s1]+spinstate[s2]+spinstate[s3]) for c, (s1,s2,s3) in enumerate(c2s)], dtype = 'int8')
+ #print(sum(abs(cvals)==3)/len(c2s))
     return cvals
     
 # In[ ]:
