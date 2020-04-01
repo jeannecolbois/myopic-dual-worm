7b653b181b66b4317f78a7af9180575a7bdcf7f8
Jeanne Colbois <jeanne.colbois@gmail.com>
2020-04-01 19:34:16 +0200

    Proper renaming of the setup files

--- dimers/setup.py
+++ dimers/setup.py
@@ -5,7 +5,13 @@ from numpy.distutils.core import setup
 from numpy.distutils.core import Extension
 
 cext = Extension(name = "dimers",
-                sources=["hamiltonian.cpp", "updatespinstates.cpp", "magneticdualworms.cpp", "measupdate.cpp", "ssf.cpp", "ssfsevolve.cpp","mcsevolve.cpp", "manydualworms.cpp", "magneticmcsevolve.cpp", "dimers.cpp"],
+                sources=["hamiltonian.cpp", 
+		"updatespinstates.cpp", "measupdate.cpp",
+		"magneticmcsevolve.cpp","magneticdualworms.cpp",
+		"ssf.cpp", "ssfsevolve.cpp",
+		"ssf_gen.cpp","genssfsevolve.cpp",
+		"mcsevolve.cpp", "manydualworms.cpp",
+                "dimers.cpp"],
                 extra_compile_args=['-std=c++11', '-fopenmp'],
                 include_dirs = [np.get_include()],
                 extra_link_args=['-lgomp'],
