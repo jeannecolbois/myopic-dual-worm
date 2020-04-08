16982f5498ed00026e6ce2cbb58db06e8f5e92b2
Jeanne Colbois <jeanne.colbois@gmail.com>
2020-04-08 16:01:21 +0200

    Introducing the MFU computation

---
+++ dualworm-kagome/MFU.m
@@ -0,0 +1,16 @@
+function [configurations] = RequiredConfigurations(unit, foldername)
+% [configurations] = RequiredConfigurations(unit, foldername)
+% For a unit cell "unit" and a list of ground state configurations "foldername"
+% this function returns the configurations on the unit cell which are
+% expected to be in the ground state of a proper MFU hamiltonian on this unit cell
+
+% 1 - Load the folder family by family
+listing = dir
+
+% 2 - For each family, find all the unit cell configurations which are allowed
+% (given the basis)
+
+% 3 - Check that the families are different
+
+
+end
